e73042b84632970b58d3ac44e2505f170d57a818
ivonneandrea <ivonne.bn@gmail.com>
2024-06-27 20:48:13 -0400

    adding database

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { logger } from "logger-express";
+
+import joyasRoute from "./routes/joyas.route.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(logger());
+app.use("/joyas", joyasRoute);
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+});
